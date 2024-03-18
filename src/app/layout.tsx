698f62bf7e5ba54202e43408c3573fc5334505ef
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2024-03-18 01:24:35 -0400

    feat: add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/header";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <GoogleAnalytics gaId="G-17QZQ3SD4G" />
       <body className={inter.className + " bg-black text-white"}>
         <Header />
         <div className="mt-[72px]">{children}</div>
