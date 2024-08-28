767d980faa37bffd3f283df5fb2584b4ce9cc73d
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2024-08-28 13:59:25 -0400

    update Stock Manager status

--- src/app/portfolio/st-landing/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function STLanding() {
-  return <main></main>;
-}
--- src/app/portfolio/stock-manager/page.tsx
+++ src/app/portfolio/stock-manager/page.tsx
@@ -33,22 +33,53 @@ export default function StockManager() {
           />
           <BackLogItem
             name="Company registration"
-            status="Doing"
+            status="Done"
             description="The master administrator must be able to register a company"
             tasks={[
-              { name: "[Frontend] Form page", status: "To do" },
-              { name: "[Backend] API route", status: "To do" },
+              { name: "[Frontend] Form page", status: "Done" },
+              { name: "[Backend] API route", status: "Done" },
             ]}
           />
           <BackLogItem
             name="User registration"
-            status="To do"
+            status="Done"
             description="The master administrator must be able to generate a registration link to the company manager where the manager must register personally. The link must be sent automatically via email. The registration link can only work once"
             tasks={[
-              { name: "[Frontend] Initial form", status: "To do" },
-              { name: "[Backend] E-mail sender", status: "To do" },
-              { name: "[Frontend] User form", status: "To do" },
+              { name: "[Frontend] Initial form", status: "Done" },
+              { name: "[Backend] E-mail sender", status: "Done" },
+              { name: "[Frontend] User form", status: "Done" },
             ]}
+          />
+          <BackLogItem
+            name="Products, stations and suppliers CRUDs"
+            status="Done"
+            description="Should be possible create, read, update and delete Products, stations and suppliers from database"
+            tasks={[
+              { name: "[Frontend] Pages", status: "Done" },
+              { name: "[Backend] Logical management", status: "Done" },
+            ]}
+          />
+          <BackLogItem
+            name="Material requisition system"
+            status="Doing"
+            description="The user should create a material requisition, where automatically the system check the inventory to get the difference and send to buyer"
+            tasks={[
+              { name: "[Frontend] Requester page", status: "Done" },
+              { name: "[Backend] Logical management", status: "Doing" },
+              { name: "[Backend] Integration with inventory", status: "To do" },
+              { name: "[Frontend] Buyer page", status: "To do" },
+              {
+                name: "[Backend] data distribution and status filter",
+                status: "To do",
+              },
+              { name: "[Frontend] Storekeeper page", status: "To do" },
+            ]}
+          />
+          <BackLogItem
+            name="Budget forecast"
+            status="To do"
+            description="Upon receiving a material request, the buyer must be able to generate a report with a preliminary budget based on the latest purchase values ​​and suppliers of the requested items"
+            tasks={[{ name: "Not described yet", status: "To do" }]}
           />
         </section>
       </div>
